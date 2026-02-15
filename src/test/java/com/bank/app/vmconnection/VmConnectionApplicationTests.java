d7c4a73bbc2430f0e0b3d16cebfe70daf9682c04
Commander-Ayush <ayushkrjha221004@gmail.com>
2026-02-15 23:32:34 +0530

    first commit

---
+++ src/test/java/com/bank/app/vmconnection/VmConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.app.vmconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
